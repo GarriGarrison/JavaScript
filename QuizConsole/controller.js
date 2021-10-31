ccf4b94baa74b08ad8f46192ce422fe6c19c15ba
Igor Ermakov <77785633+GarriGarrison@users.noreply.github.com>
2021-10-31 10:41:05 +0300

    Версия 1.0
    Управление игрой (запуск программы, выбор темы, подсчёт очков и т.д.)

---
+++ QuizConsole/controller.js
@@ -0,0 +1,41 @@
+class Controller {
+  constructor(model, view) {
+    this.model = model
+    this.view = view
+    this.newGame = '0'
+    this.game = []
+    this.score = 0
+    this.totalScore = 0
+  }
+
+  async run() {
+    await this.topicsMenu()  
+    this.game = await this.model.playGame(this.newGame)
+    this.totalScore = this.game.length
+
+    for (let i = 0; i < this.game.length; i++) {
+      const answer = await this.view.printInput('\n' + this.game[i].question)
+      if (answer.toLowerCase() === this.game[i].answer.toLowerCase()) {
+        this.score++
+        this.view.printOutput(true)
+      }
+      else
+        this.view.printOutput(false)
+    }
+    this.printEnd()
+  }
+
+  async topicsMenu() {
+    let themeList = await this.model.readTopics()
+   // console.log(themeList);
+    this.newGame = await this.view.printInput('Выберите тему: \n' + themeList)
+  }
+
+  printEnd() {
+    let percent = Math.floor(this.score * 100 / this.totalScore)
+    this.view.printResults(`${percent}`)
+  }
+
+}
+
+module.exports = Controller
