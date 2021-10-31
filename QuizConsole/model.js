7e6c54ccd816aad21c95127855e3aafc4efbe3d8
Igor Ermakov <77785633+GarriGarrison@users.noreply.github.com>
2021-10-31 10:39:12 +0300

    Версия 1.0
    Считывает папку с файлами игр и запускает выбранную игру (файл)

---
+++ QuizConsole/model.js
@@ -0,0 +1,50 @@
+const fs = require('fs').promises
+
+class Model {
+  constructor(){
+    this.gamesWay = './topics'
+    this.gamesFile = {}
+  }
+
+  readTopics() {
+    return fs.readdir(this.gamesWay).then((data) => {
+      for (let i = 1; i <= data.length; i++) {
+        this.gamesFile[i] = data[i-1]
+      }
+      
+      //return data.join('\n').replace(/_flashcard_data.txt/gim, '')  //сборка тем в одну строку без нумерации
+      let themeStr = ''
+      for (let i = 0; i < data.length; i++) {
+        themeStr += `${i+1}. ${data[i].replace(/_flashcard_data.txt/gim, '')}\n`
+      }
+      return themeStr
+    })
+  }
+
+  playGame(numGame) {
+    let playGame = this.gamesFile[numGame]
+
+    return fs.readFile(`${this.gamesWay}/${playGame}`, 'utf8').then((data) => {
+      let arr = data.split('\n')
+      let arr2 = []
+      let result = []
+      let obj2 = {}
+      for (let i = 0; i < arr.length; i += 2) {
+        if (arr[i] != undefined && arr[i + 1] != undefined) {
+          arr2.push([arr[i], arr[i + 1]])
+        }
+      }
+      
+      for (let i = 0; i < arr2.length; i++) {
+        let newOb = {
+          question: arr2[i][0],
+          answer: arr2[i][1],
+        }
+        result.push(newOb)
+      }   
+      return result
+    })
+  }
+}
+
+module.exports = Model
