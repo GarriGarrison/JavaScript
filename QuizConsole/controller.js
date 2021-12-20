4e43bdc9d270c9b7010d0e80de36ac2fb17382fe
Igor Ermakov <77785633+GarriGarrison@users.noreply.github.com>
2021-12-20 18:22:15 +0300

    Версия 1.1
    Убран cjnsole.log

--- QuizConsole/controller.js
+++ QuizConsole/controller.js
@@ -27,7 +27,6 @@ class Controller {
 
   async topicsMenu() {
     let themeList = await this.model.readTopics()
-   // console.log(themeList);
     this.newGame = await this.view.printInput('Выберите тему: \n' + themeList)
   }
 
